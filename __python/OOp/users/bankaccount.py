2ff68f1eeb2842fc7eda3840cd1944fdc75fb69f
momenkittaneh <momen.kittaneh93@outlook.com>
2021-05-10 21:00:26 +0300

    users

--- __python/OOp/users/bankaccount.py
+++ __python/OOp/users/bankaccount.py
@@ -1,17 +1,20 @@
 class BankAccount:
     def __init__(self,int_rate,balance):
-        int_rate = int_rate
-        balance = balance
+        self.int_rate = int_rate
+        self.balance = balance
+
     def deposit(self,amount):
-        balance += amount
+        self.balance+=amount
     def withdraw(self,amount):
         if amount<balance:
-            balance -= amount
+            self.balance -= amount
         else:
             print(f"Insufficient funds: Charging a ${amount} fee and deduct ${balance}")
     def yield_interest(self):
-        if balance>0:
-            balance += balance * int_rate
+        if self.balance>0:
+            self.balance += self.balance * self.int_rate
+    def display_account_info(self):
+        print(f"account balance= {self.balance} and the benfit= {self.int_rate}")
 
 bank1= BankAccount(10,1000)
 bank2= BankAccount(5,2000)
