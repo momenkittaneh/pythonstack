aacc2986b68c023cc70ee66016f3ca60459f75dd
momenkittaneh <momen.kittaneh93@outlook.com>
2021-05-10 14:35:20 +0300

    for loop

---
+++ __python/python_fundamentals/for loop basic 1/for loop basic.py
@@ -0,0 +1,33 @@
+#1
+for x in range(150):
+    print(x)
+
+#2
+for x in range(5,1000):
+    print(x*5)
+
+#3
+for x in range(1,101):
+    if x%5 == 0:
+        print("coding",end=" ")
+    if 10%10 == 0:
+        print("dojo")
+    else:
+        print(x)
+#4
+sum=0
+for x in range(500000):
+    if x%2 !=0:
+        sum +=x
+#5
+for x in range(2018,-4):
+    print(x)
+
+#6
+lownumber= "first number"
+highnumber ="last number"
+mult = "increment number"
+Begin=lownumber +1 
+for x in range(Begin,hignumber+1,mult):
+    print(Begin*mult)
+
