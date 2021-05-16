109575d03eae97e55864fbbd3d390ac8ab4c0023
momenkittaneh <momen.kittaneh93@outlook.com>
2021-05-16 11:24:23 +0300

    playground

--- flask/playground/playground.py
+++ flask/playground/playground.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
-app = Flask(__name__)                     
-    
-@app.route('/play/')
-@app.route("/play/<int:x>")
-@app.route("/play/<color>")  
-@app.route("/play/<int:x>/<color>")                         
-def play(x=3,color="blue"):
-    return render_template("index.html",x=x,color=color)
+from flask import Flask,render_template
 
-    
+app = Flask(__name__)                     
+
+@app.route("/")
+@app.route("/<int:x>")
+@app.route("/<int:x>/<int:y>")
+@app.route("/<color>")
+@app.route("/<color>/<color2>")
+@app.route("/<int:x>/<color>")
+@app.route("/<int:x>/<int:y>/<color>")
+@app.route("/<int:x>/<int:y>/<color>/<color2>")
+def home(x=8,y=8,color="red",color2="black"):
+    return render_template("index.html",x=x,y=y,color=color,color2=color2)
+
 if __name__=="__main__":
     app.run(debug=True) 
